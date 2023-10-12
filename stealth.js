8b254fbe4e2e784ae4bebfd4092adc2191a325d9
DatSwede <88003385+DatSwede@users.noreply.github.com>
2023-10-12 14:31:08 -0700

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.querySelector('form'); // this selects the first form on the page
+    var form = document.getElementById('form-1');
     var stealthInput = document.getElementById('stealth');
 
-    form.addEventListener('submit', function(e) {
-        if (stealthInput.value.trim() !== '') { // if the stealth input is not empty
-            e.preventDefault(); // prevent the form from submitting
-            alert('Form submission prevented due to stealth input being filled out.'); // optional: show a message
-        }
-    });
+    if(form && stealthInput) {
+        form.addEventListener('submit', function(e) {
+            if (stealthInput.value.trim() !== '') { // if the stealth input is not empty
+                e.preventDefault(); // prevent the form from submitting
+                alert('Form submission prevented due to stealth input being filled out.'); // optional: show a message
+            }
+        });
+    }
 });
